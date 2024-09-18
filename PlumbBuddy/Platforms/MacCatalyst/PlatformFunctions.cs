28e78f1f2a910e5aa6cf7a461c38fa7a8cb5bf69
Daniel Henry <daniel.henry@epiforge.com>
2024-09-17 21:13:40 -0400

    come on, macOS...

--- PlumbBuddy/Platforms/MacCatalyst/PlatformFunctions.cs
+++ PlumbBuddy/Platforms/MacCatalyst/PlatformFunctions.cs
@@ -3,14 +3,39 @@ namespace PlumbBuddy.Platforms.MacCatalyst;
 class PlatformFunctions :
     IPlatformFunctions
 {
+    static readonly TimeSpan gameProcessScanGracePeriod = TimeSpan.FromSeconds(5);
+
+    DateTimeOffset? lastGameProcessScan;
+
     public StringComparison FileSystemStringComparison =>
         StringComparison.Ordinal;
 
     public async Task<Process?> GetGameProcessAsync(DirectoryInfo installationDirectory)
     {
         ArgumentNullException.ThrowIfNull(installationDirectory);
-        var gameProcess = Process.GetProcesses().Where(process => process.ProcessName.Contains("sims", StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
-        return gameProcess;
+        if (lastGameProcessScan is { } last
+            && last + gameProcessScanGracePeriod > DateTimeOffset.UtcNow)
+            return null;
+        lastGameProcessScan = DateTimeOffset.UtcNow;
+        using var zshProcess = Process.Start(new ProcessStartInfo("/bin/zsh", $"-c \"pgrep -f 'The Sims 4'\"")
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        });
+        if (zshProcess is not null)
+        {
+            using var zshOutputReader = zshProcess.StandardOutput;
+            var zshOutput = await zshOutputReader.ReadToEndAsync().ConfigureAwait(false);
+            var maybeNullProcessId = zshOutput
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(line => int.TryParse(line, out var processId) ? processId : (int?)null)
+                .Where(maybeNullProcessId => maybeNullProcessId is not null)
+                .FirstOrDefault();
+            if (maybeNullProcessId is { } processId)
+                return Process.GetProcessById(processId);
+        }
+        return null;
     }
 
     public void ViewDirectory(DirectoryInfo directoryInfo) =>
