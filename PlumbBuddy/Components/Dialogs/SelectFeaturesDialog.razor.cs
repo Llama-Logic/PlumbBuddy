5926daf7dce277ad0e5d9ef5c28494e4b7421206
Daniel Henry <daniel.henry@epiforge.com>
2024-11-01 20:02:03 -0400

    Select Features warns if no features were moved to required

--- PlumbBuddy/Components/Dialogs/SelectFeaturesDialog.razor.cs
+++ PlumbBuddy/Components/Dialogs/SelectFeaturesDialog.razor.cs
@@ -14,8 +14,12 @@ partial class SelectFeaturesDialog
     void CancelOnClickHandler() =>
         MudDialog?.Close(DialogResult.Cancel());
 
-    void OkOnClickHandler() =>
+    async Task OkOnClickHandlerAsync()
+    {
+        if (requiredFeatures?.Count is 0 && !await DialogService.ShowCautionDialogAsync("Proceeding with No Required Features", "You didn't move any available features to the required section. If this was a mistake, click Cancel and you can do so."))
+            return;
         MudDialog?.Close(DialogResult.Ok(requiredFeatures?.ToImmutableArray()));
+    }
 
     protected override void OnParametersSet()
     {
