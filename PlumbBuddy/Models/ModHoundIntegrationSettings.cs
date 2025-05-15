5107e6f1d3e9fc27e539f9c7c4eedcec2d1d73c5
Daniel Henry <daniel.henry@gmail.com>
2025-05-15 01:42:30 -0400

    #36 because it's common sense

--- PlumbBuddy/Models/ModHoundIntegrationSettings.cs
+++ PlumbBuddy/Models/ModHoundIntegrationSettings.cs
@@ -2,8 +2,8 @@ namespace PlumbBuddy.Models;
 
 public class ModHoundIntegrationSettings
 {
-    public int PackagesBatchHardLimit { get; set; }
-    public int PackagesBatchWarningThreshold { get; set; }
-    public TimeSpan ReportServiceTimeout { get; set; }
+    public int PackagesBatchHardLimit { get; set; } = 26000;
+    public int PackagesBatchWarningThreshold { get; set; } = 5000;
+    public TimeSpan ReportServiceTimeout { get; set; } = TimeSpan.FromMinutes(10);
     public string? ReportServiceUnavailableMessage { get; set; }
 }
