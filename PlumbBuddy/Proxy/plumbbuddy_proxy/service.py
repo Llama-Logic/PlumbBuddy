07edaeea1e72c7cce8fb58dd52c3acf45969e736
Daniel Henry <daniel.henry@gmail.com>
2025-06-08 18:36:27 -0400

    #42

--- PlumbBuddy/Proxy/plumbbuddy_proxy/service.py
+++ PlumbBuddy/Proxy/plumbbuddy_proxy/service.py
@@ -6,68 +6,82 @@ from plumbbuddy_proxy.api import gateway
 import services
 from sims4.service_manager import Service
 
+def _attach_save_characteristics(ipc_message: dict) -> dict:
+    try:
+        persistence = services.get_persistence_service()
+        account = persistence.get_account_proto_buff()
+        return {
+            **ipc_message,
+            'nucleus_id': account.nucleus_id,
+            'created': account.created,
+            'sim_now': int(services.time_service().sim_now)
+        }
+    except Exception as ex:
+        logger.exception(ex)
+        return ipc_message
+
 class PlumbBuddyProxyService(Service):
     def save(self, *args, **kwargs):
-        ipc.send({
-            "type": "game_service_message",
-            "name": "save"
-        })
+        ipc.send(_attach_save_characteristics({
+            'type': 'game_service_message',
+            'name': 'save'
+        }))
 
     def pre_save(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "pre_save"
+            'type': 'game_service_message',
+            'name': 'pre_save'
         })
 
     def setup(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "setup"
+            'type': 'game_service_message',
+            'name': 'setup'
         })
 
     def load(self, *args, **kwargs):
-        ipc.send({
-            "type": "game_service_message",
-            "name": "load"
-        })
+        ipc.send(_attach_save_characteristics({
+            'type': 'game_service_message',
+            'name': 'load'
+        }))
 
     def start(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "start"
+            'type': 'game_service_message',
+            'name': 'start'
         })
 
     def stop(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "stop"
+            'type': 'game_service_message',
+            'name': 'stop'
         })
 
     def on_zone_load(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "on_zone_load"
+            'type': 'game_service_message',
+            'name': 'on_zone_load'
         })
 
     def on_zone_unload(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "on_zone_unload"
+            'type': 'game_service_message',
+            'name': 'on_zone_unload'
         })
 
     def on_cleanup_zone_objects(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "on_cleanup_zone_objects"
+            'type': 'game_service_message',
+            'name': 'on_cleanup_zone_objects'
         })
 
     def on_all_households_and_sim_infos_loaded(self, *args, **kwargs):
         ipc.send({
-            "type": "game_service_message",
-            "name": "on_all_households_and_sim_infos_loaded"
+            'type': 'game_service_message',
+            'name': 'on_all_households_and_sim_infos_loaded'
         })
 
-@inject_to(GameServiceManager, "start_services")
+@inject_to(GameServiceManager, 'start_services')
 def _start_services(original, self, *args, **kwargs):
     try:
         self.register_service(PlumbBuddyProxyService(), is_init_critical = False)
