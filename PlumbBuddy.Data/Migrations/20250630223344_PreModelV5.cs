fe0ebb9ffaa6589bdbd6a4f48d82bcbfa9449116
Daniel Henry <daniel.henry@gmail.com>
2025-06-30 18:36:38 -0400

    ModelV5 prep

---
+++ PlumbBuddy.Data/Migrations/20250630223344_PreModelV5.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+#pragma warning disable IDE0079
+#pragma warning disable CA1062
+#pragma warning disable CA1861
+#pragma warning disable IDE0053
+#pragma warning disable IDE0161
+#pragma warning disable IDE0300
+
+namespace PlumbBuddy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PreModelV5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) =>
+            migrationBuilder.Sql("UPDATE ModFileHash SET ResourcesAndManifestsCataloged = 0;");
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
