37d722eb6181d055e036be840bbe765a165f3ecb
Daniel Henry <daniel.henry@gmail.com>
2025-09-09 19:42:13 -0400

    switch to new cert; re-sign manually for timestamp

--- PlumbBuddy/Build.ps1
+++ PlumbBuddy/Build.ps1
@@ -27,4 +27,10 @@ Move-Item -Path ${outputDir}PlumbBuddy_${version}.0_Test\PlumbBuddy_${version}.0
 Write-Progress -Activity "Build for Windows" -Status "Removing $version Workspace" -PercentComplete 80
 Remove-Item -Path ${outputDir}PlumbBuddy_${version}.0_Test -Recurse
 
+Write-Progress -Activity "Build for Windows" -Status "Timestamping x64" -PercentComplete 87
+signtool sign /fd SHA256 /tr http://timestamp.sectigo.com /td SHA256 /sha1 0c3a6be3d44b381a2185f90423fcc567a6cb4338 "${outputDir}PlumbBuddy_${version}_x64.msix"
+
+Write-Progress -Activity "Build for Windows" -Status "Timestamping amd64" -PercentComplete 94
+signtool sign /fd SHA256 /tr http://timestamp.sectigo.com /td SHA256 /sha1 0c3a6be3d44b381a2185f90423fcc567a6cb4338 "${outputDir}PlumbBuddy_${version}_arm64.msix"
+
 Write-Progress -Activity "Build for Windows" -Completed -Status "Built $version for x64 and arm64"
